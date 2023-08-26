00bcdde2633554862a10060101d869b4c4961498
jahanzeb011 <99148364+jahanzeb011@users.noreply.github.com>
2023-08-26 16:41:29 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,7 @@
 // Go to the home page when the button is clicked
 document.getElementById('goToHomePage').addEventListener('click', () => {
     window.location.href = 'home.html'; // Replace 'home.html' with your home page URL
+    const utterance = new SpeechSynthesisUtterance("Welcome to Prime Number Generator.");
+    speechSynthesis.speak(utterance);
 });
 
